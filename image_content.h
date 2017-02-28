dd8b3dfd366d9c2bfa7ffeb768b8b4a30ddde3d4
tyliao <smog70151@gmail.com>
2017-02-28 17:26:50 +0800

    halt can action

--- image_content.h
+++ image_content.h
@@ -3,9 +3,4 @@
 
 using namespace std;
 
-typedef struct image_content{
-    string data_inst ;
-    image_content *prev = nullptr;
-    image_content *next = nullptr;
-}image_content;
 
--- main.cpp
+++ main.cpp
@@ -3,18 +3,20 @@
 #include <fstream>
 #include <string>
 
+#include "image_content.h"
+
 using namespace std;
 
-unsigned int inst_data(unsigned int inst, int pow, char value)
+unsigned int inst_data(unsigned int inst, int pow,unsigned char value)
 {
     if(pow==4)
-        inst+=value/16*(16*16*16)*(16*16*16*16)+value%16*(16*16*16)*(16*16*16);
+        inst=(value>>4<<28)+((value%16)<<24);
     else if(pow==3)
-        inst+=value/16*16*16*16*16*16+value%16*16*16*16*16;
+        inst+=(value>>4<<20)+((value%16)<<16);
     else if(pow==2)
-        inst+=value/16*16*16*16+value%16*16*16;
+        inst+=(value>>4<<12)+((value%16)<<8);
     else if(pow==1)
-        inst+=value/16*16+value%16;
+        inst+=(value>>4<<4)+value%16;
     return inst;
 }
 
@@ -24,6 +26,7 @@ int main()
     iimage.open("iimage.bin",ios::in);
     unsigned char c;
     unsigned int inst=0;
+    unsigned int count;
     int i=0;
 
     //initialize cyc 0 (iimage_0)
@@ -34,13 +37,28 @@ int main()
         inst=inst_data(inst,4-i,c);
     }
     cout << setw(8) << setfill('0')<< hex << inst << endl ;
+    //number of instructions (iimage_1)
+    for(int i=0; i<4; i++)
+    {
+        if(i==0) inst = 0;
+        iimage.read((char*)&c,sizeof(char));
+        inst=inst_data(inst,4-i,c);
+    }
+    cout << setw(8) << setfill('0')<< hex << inst << endl ;
+    count = inst;
     //(iimage_2~end)
-    while(iimage.read((char*)&c,sizeof(char)))
+    while(count)
     {
         for(int i=0; i<4; i++)
         {
+            if(i==0) inst = 0;
             iimage.read((char*)&c,sizeof(char));
+            inst=inst_data(inst,4-i,c);
         }
+        cout << setw(8) << setfill('0')<< hex << inst << endl ;
+        count--;
+        if((inst>>26)==63)
+            break;
     }
     iimage.close();
     return 0;
