27af062cba9efbfa87ba898e85ad45d3bb8c4621
tyliao <smog70151@gmail.com>
2017-02-28 16:22:18 +0800

    unsigned_instruction

--- main.cpp
+++ main.cpp
@@ -1,45 +1,46 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 
-#include "register_content.h"
-
 using namespace std;
+
+unsigned int inst_data(unsigned int inst, int pow, char value)
+{
+    if(pow==4)
+        inst+=value/16*(16*16*16)*(16*16*16*16)+value%16*(16*16*16)*(16*16*16);
+    else if(pow==3)
+        inst+=value/16*16*16*16*16*16+value%16*16*16*16*16;
+    else if(pow==2)
+        inst+=value/16*16*16*16+value%16*16*16;
+    else if(pow==1)
+        inst+=value/16*16+value%16;
+    return inst;
+}
 
 int main()
 {
     fstream iimage;
-    snapshot snap;
     iimage.open("iimage.bin",ios::in);
     unsigned char c;
+    unsigned int inst=0;
     int i=0;
 
     //initialize cyc 0 (iimage_0)
     for(int i=0; i<4; i++)
     {
+        if(i==0) inst = 0;
         iimage.read((char*)&c,sizeof(char));
-        snap.alter_inst(0,34,i,c);
+        inst=inst_data(inst,4-i,c);
     }
-    snap.cycle(0);
-
-    //(iimage_1)
-    snap.alter_inst(1,34,4,c);
-    snap.instruction[34][7]+=4;
-    for(int i=0; i<4; i++)
-    {
-        iimage.read((char*)&c,sizeof(char));
-    }
-    snap.cycle(1);
-
+    cout << setw(8) << setfill('0')<< hex << inst << endl ;
     //(iimage_2~end)
     while(iimage.read((char*)&c,sizeof(char)))
     {
         for(int i=0; i<4; i++)
         {
             iimage.read((char*)&c,sizeof(char));
-            //snap.alter_inst(34,i,c);
         }
-        //snap.cycle(0);
     }
     iimage.close();
     return 0;
